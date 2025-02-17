89a4be293d286f0fbb0354fe7001b24e7e87cfe7
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2025-02-17 08:38:15 -0300

    Introdução API REST

---
+++ alunos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Estado, Cidade, Aluno
+
+admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Aluno)
+
+
+# Register your models here.
